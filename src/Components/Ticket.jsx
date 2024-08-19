91b7d87b371050510efa1cd9039148015938c51c
Satish <satishpati96548@gmail.com>
2024-08-19 07:24:47 +0530

    updated

--- src/Components/Ticket.jsx
+++ src/Components/Ticket.jsx
@@ -9,16 +9,16 @@ const Ticket = ({ eventName, eventDate, ticketNumber, eventImage, venue, locatio
     });
 
     return (
-        <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-8 rounded-lg shadow-lg border border-transparent flex items-center mb-8 w-full max-w-5xl">
-            <div className="flex-shrink-0 w-48 h-48">
+        <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-6 sm:p-8 rounded-lg shadow-lg border border-transparent flex flex-col sm:flex-row items-center mb-8 w-full max-w-5xl">
+            <div className="flex-shrink-0 w-full sm:w-48 h-48 mb-4 sm:mb-0">
                 <img src={eventImage} alt={`${eventName} poster`} className="w-full h-full object-cover rounded-lg shadow-lg" />
             </div>
-            <div className="ml-8 flex-grow">
-                <h3 className="text-4xl font-bold text-white mb-4">{eventName}</h3>
-                <p className="text-lg text-gray-100 mb-2">Date: {formattedDate} </p>
-                <p className="text-lg text-gray-100 mb-2">Time: {time}</p>
-                <p className="text-lg text-gray-100 mb-2">Ticket #: {ticketNumber}</p>
-                <p className="text-lg text-gray-100">Venue: {venue}, {location}</p>
+            <div className="sm:ml-8 flex-grow text-center sm:text-left">
+                <h3 className="text-2xl sm:text-4xl font-bold text-white mb-4">{eventName}</h3>
+                <p className="text-md sm:text-lg text-gray-100 mb-2">Date: {formattedDate} </p>
+                <p className="text-md sm:text-lg text-gray-100 mb-2">Time: {time}</p>
+                <p className="text-md sm:text-lg text-gray-100 mb-2">Ticket #: {ticketNumber}</p>
+                <p className="text-md sm:text-lg text-gray-100">Venue: {venue}, {location}</p>
             </div>
         </div>
     );
